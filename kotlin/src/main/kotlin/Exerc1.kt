853ac43d846daa6dd3af6561cb27383afeef5ac8
Eduardo <duedugaranito@gmail.com>
2023-08-01 12:30:45 -0300

    Exercicio 1

--- kotlin/src/main/kotlin/Exerc1.kt
+++ kotlin/src/main/kotlin/Exerc1.kt
@@ -2,10 +2,10 @@
  *https://github.com/DevMasterTeam/Udemy-Andorid-Kotlin/blob/master/Secao%2005%20-%20Conceitos%20iniciais/Reforce%20seu%20aprendizado%20-%20Vari%C3%A1veis%2C%20tipos%20de%20dado%20e%20operadores.pdf
  */
 fun main() {
-    var nome :String = "Eduardo Garanito Pinheiro"
-    var texto:String
-    val calcado:Byte = 42
-    val pib:ULong = 1869000000000u
-    val pop:UInt = 211000000u
-    println("O valor do pib per capita é: "+ (pib/pop))
+    var nome: String = "Eduardo Garanito Pinheiro"
+    var texto: String
+    val calcado: Byte = 42
+    val pib: ULong = 1869000000000u
+    val pop: UInt = 211000000u
+    println("O valor do pib per capita é: " + (pib / pop))
 }
